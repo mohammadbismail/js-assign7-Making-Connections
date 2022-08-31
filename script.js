c64fe28a27fe42e64384483d0d4f22205ad5c685
mohammadbisamil <m.basim.ismail@gmail.com>
2022-08-31 11:32:35 +0300

    mod

--- script.js
+++ script.js
@@ -2,18 +2,24 @@ var tod = document.querySelector("#todd");
 var phil = document.querySelector("#phil");
 var mismail = document.getElementById("ismail");
 var clicked = [1, 2]; //index 0,1 for tod and phil
-var badge = document.querySelector(".badge");
+var badge = document.querySelector(".vol");
+var badge2 = document.querySelector(".conn");
 var count = 500;
+var count2 = 2;
 console.log(tod, phil, badge, clicked);
 function remove(i) {
   if (clicked[i] == 1) {
     tod.remove();
-    count--;
-    badge.innerHTML = count;
+    count++;
+    count2--;
+    badge.innerHTML = count; ///"<h1>hi</h1>"
+    badge2.innerHTML = count2;
   } else if (clicked[i] == 2) {
     phil.remove();
-    count--;
+    count++;
+    count2--;
     badge.innerHTML = count;
+    badge2.innerText = count2;
   }
 }
 console.log(mismail);
